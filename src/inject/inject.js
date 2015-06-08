16b22d2c3e9959bb31fe363d5309382b6337f0d3
Griffin Smith <wildgriffin45@gmail.com>
2015-06-07 20:38:50 -0400

    wip

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,23 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+      console.log("Hello. This message was sent from scripts/inject.js");
 
-	}
-	}, 10);
+      var inputs = document.querySelectorAll('.fbDockChatTabFlyout textarea');
+
+      for (var i = 0; i < inputs.length; i++) {
+        var input = inputs[i];
+        var attr = input.attributes.getNamedItem('onkeydown')
+        var prevOnKeyDown = attr.value;
+
+        input.attributes.setNamedItem(attr, '');
+        input.onkeydown = function(e) {
+          e.stopPropagation();
+          console.log('test');
+        };
+      }
+    }
+  }, 10);
 });
